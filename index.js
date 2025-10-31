613f18e75b3293a6c743a90ab74dd0e0fd669959
Abdul-rehman-rgb <abdul4758rehman@gmail.com>
2025-10-31 23:04:40 +0500

    add

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false, // Set to true in production with HTTPS
+    secure: true, // Set to true in production with HTTPS
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
